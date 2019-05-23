3b5a7fc88e396add439bae6fd63ff5a1cf080051
Blake Rose <bdecktor@gmail.com>
2019-05-23 20:36:57 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,7 @@
 def reverse_each_word(string)
-  split_string = string.split("")
-  reversed = []
-  string.size.times {reversed << split_string.pop}
-  reversed.join
+  string_split = string.split("")
+  string_split.each do |item|
+    item.reverse
+  end 
+  
 end
